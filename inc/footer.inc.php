cd8d21618da3ef08b80b2d86afb1203e80c33d81
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-05-31 20:30:47 +0200

    added footer

---
+++ inc/footer.inc.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <p class="footer--copyright">Copyright Filar 2021</p>
+    <ul class="footer__list">
+        <li class="footer__item">
+            <a href="#" class="footer__link"><img src="#" alt="facebook"></a>
+        </li>
+        <li class="footer__item">
+            <a href="#" class="footer__link"><img src="#" alt="instagram"></a>
+        </li>
+    </ul>
+</footer>
