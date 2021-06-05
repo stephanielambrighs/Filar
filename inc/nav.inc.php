f8394a52850ade0e922ab2f78e9c9fd289146396
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-06-05 21:42:21 +0200

    added nav + responsive

--- inc/nav.inc.php
+++ inc/nav.inc.php
@@ -1,28 +1,28 @@
 <nav class="main-nav" aria-label="Main">
   <ul class="main-nav__list">
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link"><img src="#" alt="logo"></a>
+      <a href="index.php" class="main-nav__link"><img class="main-nav__image" src="/images/logo_filar-white.png" alt="logo"></a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link">Home</a>
+      <a href="index.php" class="main-nav__link">Home</a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link">Ons filament</a>
+      <a href="ons-filament.php" class="main-nav__link">Ons filament</a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link">Over ons</a>
+      <a href="over-ons.php" class="main-nav__link">Over ons</a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link">Profiel</a>
+      <a href="login.php" class="main-nav__link">Profiel</a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link">Contact</a>
+      <a href="contact.php" class="main-nav__link">Contact</a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link">Winkel</a>
+      <a href="winkel.php" class="main-nav__link">Winkel</a>
     </li>
     <li class="main-nav__item">
-      <a href="#" class="main-nav__link"><img src="#" alt="winkelmand"></a>
+      <a href="winkelmand.php" class="main-nav__link"><img class="main-nav__icons" src="/images/icons_winkelmand-white.png" alt="winkelmand"></a>
     </li>
   </ul>
 </nav>
