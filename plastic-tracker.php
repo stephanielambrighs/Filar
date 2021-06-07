e78253106373a22bc6f44d59177cf070fe1d3d02
HungryHypnoHippo <plyusninilya97@gmail.com>
2021-06-07 19:46:39 +0200

    added chart functionality

--- plastic-tracker.php
+++ plastic-tracker.php
@@ -1,3 +1,10 @@
+<?php
+  include_once(__DIR__ . "/classes/User.php");
+
+  $u = new User();
+  $currentPlastic = $u->plasticTracker();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,22 +16,21 @@
 </head>
 <body>
 
-
 <?php include_once("inc/nav.inc.php"); ?>
 
 <nav class="sub-nav" aria-label="sub">
   <ul class="sub-nav__list">
     <li class="sub-nav__item--active">
-      <a href="#" class="sub-nav__link--active">Plastic tracker</a>
+      <a href="plastic-tracker.php" class="sub-nav__link--active">Plastic tracker</a>
     </li>
     <li class="sub-nav__item">
-      <a href="#" class="sub-nav__link">inlevergeschiedenis</a>
+      <a href="inlevergeschiedenis.php" class="sub-nav__link">Inlevergeschiedenis</a>
     </li>
     <li class="sub-nav__item">
-      <a href="#" class="sub-nav__link">Aankoopgeschiedenis</a>
+      <a href="aankoopgeschiedenis.php" class="sub-nav__link">Aankoopgeschiedenis</a>
     </li>
     <li class="sub-nav__item">
-      <a href="#" class="sub-nav__link">Profielinstellingen</a>
+      <a href="profielinstellingen.php" class="sub-nav__link">Profielinstellingen</a>
     </li>
   </ul>
 </nav>
@@ -33,16 +39,19 @@
   <div class="card__header--tracker">
   <h1 class="card__title">Plastic Tracker</h1>
     <figure class="card__figure">
-      <img src="/images/2_Sterk.png" alt="donut_chart" class="card__image--chart">
+      <!-- <img src="/images/2_Sterk.png" alt="donut_chart" class="card__image--chart"> -->
+      <p>Huidig PET doel</p>
+      <p>0,<span id="currentTarget"><?php echo $currentPlastic[1] ?></span>/1kg</p>
+      <canvas id="myChart" class="card__image--chart"></canvas>
     </figure>
     <div class="card__body--tracker">
         <div class="card__copy">
             <h2 class="card__subtitle">Bonnen ontvangen</h2>
-            <h3 class="card__subtitle--h3">3</h3>
+            <h3 class="card__subtitle--h3"><?php echo $currentPlastic[0] ?></h3>
         </div>
         <div class="card__copy">
             <h2 class="card__subtitle">Totaal PET ingeleverd</h2>
-            <h3 class="card__subtitle--h3">3,6kg</h3>
+            <h3 class="card__subtitle--h3"><?php echo $currentPlastic[0] ?>,<?php echo $currentPlastic[1] ?>kg</h3>
         </div>
         <a href="#" class="form-group__link--code">Mijn volledige inlevergeschiedenis</a>
     </div>
@@ -60,14 +69,32 @@
         Dan hoef je het alleen nog maar in te leveren en de rest van het proces nemen wij wel voor onze rekening.
         Een aantal dagen later zal je de korting in jouw profiel zien staan.
         </p>
-        <a href="#" class="button__code">
+        <a href="/documents/persoonlijke_code.pdf" class="button__code" download="persoonlijke_code">
             <span class="button__body">Download persoonlijke code</span>
         </a>
     </div>
   </div>
 </article>
 
-
 <?php include_once("inc/footer.inc.php"); ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.2/chart.min.js"></script>
+<script>
+  let done = document.getElementById("currentTarget").innerText;
+  let notDone = 10 - done;
+  let myChart = document.getElementById("myChart").getContext("2d");
+  let plasticChart = new Chart(myChart, {
+    type: 'doughnut',
+    data: {
+      labels:["Ingeleverd", "Nog in te leveren"],
+      datasets: [{
+        data: [done, notDone],
+        backgroundColor: ["#44cb7c", "#2528ba"]
+      }]
+    },
+    options: {
+      cutout: "90%"
+    }
+  })
+</script>
 </body>
 </html>
