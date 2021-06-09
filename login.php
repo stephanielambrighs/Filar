3dfe5b066517e71f1f8d790abb2b2eb3b5ea401e
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-06-09 11:11:23 +0200

    added rediract to plastic-tracker page when already loggedin

--- login.php
+++ login.php
@@ -2,7 +2,10 @@
 
 require_once(__DIR__ . "/autoload.php");
 
-if(!empty($_POST['email']) && !empty($_POST['password'])){
+session_start();
+
+if(!isset($_SESSION['id'])){
+  if(!empty($_POST['email']) && !empty($_POST['password'])){
     try {
       $user = new User();
       $user->setEmail($_POST['email']);
@@ -22,7 +25,11 @@ if(!empty($_POST['email']) && !empty($_POST['password'])){
       $error = true;
     }
 
+  }
+}else{
+  header("Location: plastic-tracker.php");
 }
+
 
 
 
--- plastic-tracker.php
+++ plastic-tracker.php
@@ -1,17 +1,17 @@
 <?php
 
-  include_once(__DIR__ . "/classes/User.php");
-  require_once(__DIR__ . "/autoload.php");
+include_once(__DIR__ . "/classes/User.php");
+require_once(__DIR__ . "/autoload.php");
 
-  session_start();
+session_start();
 
-  // if the session is not set then a redirect
-  if(!isset($_SESSION['id'])){
-    header("Location: login.php");
-  }else{
-    // else do something in this page
-    $u = new User();
-    $currentPlastic = $u->plasticTracker();
+// if the session is not set then a redirect
+if(!isset($_SESSION['id'])){
+  header("Location: login.php");
+}else{
+  // else do something in this page
+  $u = new User();
+  $currentPlastic = $u->plasticTracker();
 }
 
 ?>
