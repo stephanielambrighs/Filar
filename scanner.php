a14622ba92c8726c2f35c2dd5dc8ad64a993f6ef
HungryHypnoHippo <plyusninilya97@gmail.com>
2021-06-09 13:49:35 +0200

    added scanner functionality

---
+++ scanner.php
@@ -0,0 +1,33 @@
+<?php
+    include_once(__DIR__ . "/classes/Scanner.php");
+
+    if(!empty($_POST["submit_scan"])){
+        $sc = new Scanner();
+        $user_id = $_POST["user_id"];
+        $amount = $_POST["amount"];
+        $method = $_POST["method"];
+        $sc->addPlastic($user_id, $amount, $method);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheets/style.css">
+    <title>Winkel</title>
+</head>
+<body>
+    <p id="scan_camera_view">Hoeveelheid</p>
+    <p id="scan_user_id">Gebruiker</p>
+    <p id="scan_amount">Hoeveelheid</p>
+    <form action="" method="POST">
+        <input type="text" name="user_id" placeholder="Gebruiker ID">
+        <input type="text" name="amount" placeholder="Hoeveelheid in kg">
+        <input type="text" name="method" placeholder="Hoe het werd ingeleverd">
+        <input type="submit" name="submit_scan">
+    </form>
+</body>
+</html>
